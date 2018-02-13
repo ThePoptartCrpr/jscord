671cc79f6e9fa66a3851c85729b0e7d5cdc4e874
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-02-12 17:27:48 -0800

    Added jsdocs

--- src/util/Logger.js
+++ src/util/Logger.js
@@ -1,11 +1,24 @@
 const moment = require('moment');
 const chalk = require('chalk');
 
+/**
+ * Utility class to help streamline the process of logging information to console.
+ */
 class Logger {
+  /**
+   * Logs a message to console with a timestamp.
+   * @param  {string} obj The message you're logging to console
+   * @return {void}     
+   */
   log(obj) {
     console.log(`[${moment().format('MM-DD-YYYY HH:mm:ss')}] ${obj}`);
   }
   
+  /**
+   * Logs an error to console with a timestamp.
+   * @param  {string} obj The error you're logging to console
+   * @return {void}     
+   */
   error(obj) {
     console.log(chalk.bgRed(`[ERROR ${moment().format('MM-DD-YYYY HH:mm:ss')}]`) + ` ${obj}`);
   }
